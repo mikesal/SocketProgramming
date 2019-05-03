19a06ee6e8f956eaf655bfa34eb2b53c78a53283
Mike <mikesal318@yahoo.com>
2019-05-03 12:03:41 -0500

    Added comments

--- multi_server.py
+++ multi_server.py
@@ -16,17 +16,26 @@ def service_connect(key, mask):
 	sock = key.fileobj
 	data = key.data
 	if mask & selectors.EVENT_READ:
+		#Mask contains events that are ready.
+		#EVENT_READ indicates availability for reading.
 		recv_data = sock.recv(1024)
+		#if data is received, append it to data.outb
 		if recv_data:
 			data.outb += recv_data
-		else:
+		#If not, unregister conn and then close.
+		else: 
 			print('closing connection to', data.addr)
+			#Unreg so sock is no longer monitered by select().
 			sel.unregister(sock)
-			sock.close()
+			sock.close()			#Closes the server socket.
 	if mask & selectors.EVENT_WRITE:
+		#If mask is still open and available to be written to,
+		#and outb is not an empty string, print out contents of outb.
 		if data.outb:
 			print('echoing', repr(data.outb), 'to', data.addr)
+			#Sends contents of outb through socket to client.
 			sent = sock.send(data.outb)
+			#Deletes contents of outb.
 			data.outb = data.outb[sent:]
 
 host = '127.0.0.1'
